a3109959bb47a8ceb65052d437d64a0c22f86928
florianZac <florianaizac@msn.com>
2026-03-08 01:31:38 +0100

    création des pages mentions_légales et mentions_CGV avec css

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -13,10 +13,13 @@ export const allRoutes = [
    new Route("/inscription", "Inscription", "Pages/Auth/inscription.html", ["disconnected"], "/Script/inscription.js", true),
    new Route("/reset_login", "Resetpassword", "Pages/Auth/reset-password.html", ["disconnected"], "/Script/reset-password.js", true),
    new Route("/contact", "Contact", "Pages/Contact/contact.html", [], "/Script/contact.js", true),
+   new Route("/Mentions_legale", "Mentions_legale", "Pages/Mention_legale/Mentions_legale.html", [], "", true),
+   new Route("/Mentions_CGV", "Mentions_CGV", "Pages/Mention_legale/Mentions_CGV.html", [], "", true),
+
+
 
    new Route("/nos_menu", "Nos Menus", "Pages/Menus/nos_menus.html", [], "", true),
-   new Route("/Mentions_legale", "Mentions_legale", "Pages/Mentions_legale/Mentions_legale.html", [], "", true),
-   new Route("/Mentions_CGV", "Mentions_CGV", "Pages/Mentions_legale/Mentions_CGV.html", [], "", true),
+
    new Route("/commander", "Commander", "Pages/Commande/commander.html", ["client","employee","admin"], "", true)
 ];
 
