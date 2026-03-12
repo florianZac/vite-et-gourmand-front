455226e152ec1509409d8be3ce9d6880bc07ec55
florianZac <florianaizac@msn.com>
2026-03-12 16:47:06 +0100

    modif

--- server_prod.js
+++ server_prod.js
@@ -18,7 +18,7 @@ app.use('/api', createProxyMiddleware({
 }));
 
 // Route SPA pour le front
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
